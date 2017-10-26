9ac855a0545d8d4160e647b3d9c0d02f8228c238
Alexander Kovtunenko <akovtunenko@slice.com>
2017-10-26 10:35:59 +0300

    added test.sh

---
+++ test.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+export BASE_URI="https://api.slice.com"
+export CLIENT_ID="abcd1234"
+
+#If testing for OAuth, Uncomment following line and set appropriate OAuth access token value
+#export OAUTH_TOKEN=e2d2157c6ed09be43754d8fb1210
+
+#If testing with request token based auth, Uncomment following line and set appropriate request token value
+#export REQUEST_TOKEN=sdfsdfsdfsdfdsfdsfsdfsdfsdfsdfsdfsdfdsfds
+
+#If using debugging proxy such as Charles, uncomment following line.
+#Proxy is expected to run on localhost:8888
+#export USE_PROXY=true
+
+#------
+# Even thought it does not make sense to provide a username for OAuth and Request token based requests,
+# the parameter is required.
+# So, set it to a blank string ("")
+#
+# For some Server-side requests such as the user's collection, set username to be blank string ("")
+#------
+php slice-clt.php GET /api/v1/users ""
+
